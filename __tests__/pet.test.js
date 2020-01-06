2742eebdc2924e8f21aaac5aab36ed8dc07aebe4
nathan saide <nathansaide@nathans-MacBook-Pro.local>
2020-01-06 19:30:56 +0000

    added a growUp method...changed the value of the method on Pet's prototype, that change will occur on all instances of Pet.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -9,3 +9,42 @@ it("sets the name property", () => {
 
   expect(pet.name).toEqual("Fido");
 });
+it("sets the age property", () => {
+  const pet = new Pet();
+  expect(pet.age).toEqual(0);
+});
+describe("growUp", () => {
+  it("increments the age by 1", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+  });
+  it("sets the hunger property", () => {
+    const pet = new Pet();
+    expect(pet.hunger).toEqual(0);
+  });
+});
+describe("growUp affects the hunger", () => {
+  it("increments the hunger by 5", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.hunger).toEqual(5);
+  });
+  it("sets the fitness property", () => {
+    const pet = new Pet();
+    expect(pet.fitness).toEqual(10);
+  });
+});
+describe("growUp affects the fitness", () => {
+  it("grow up decreases the fitness by 3", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.fitness).toEqual(7);
+  });
+});
