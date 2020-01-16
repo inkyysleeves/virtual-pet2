6f2267b4ac0f8c3d28b564eadf7bc8bd6554ee6d
nathan saide <nathansaide@nathans-MacBook-Pro.local>
2020-01-16 10:48:43 +0000

    gave my pet a feed prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -66,3 +66,21 @@ describe("walk", () => {
     expect(pet.fitness).toEqual(10);
   });
 });
+describe("feed", () => {
+  it("decreases hunger by 3", () => {
+    const pet = new Pet("fido");
+
+    pet.hunger = 5;
+    pet.feed();
+
+    expect(pet.hunger).toEqual(2);
+  });
+  it("decreases hunger to a minimum of 0", () => {
+    const pet = new Pet("fido");
+
+    pet.hunger = 2
+    pet.feed();
+
+    expect(pet.hunger).toEqual(0);
+  });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,7 @@
 const pet = require("../src/pet");
 
 const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
 
 function Pet(name, age) {
   this.name = name;
@@ -12,11 +13,18 @@ function Pet(name, age) {
     this.hunger += 5;
     this.fitness -= 3;
   };
-  Pet.prototype.walk = function() {
+  Pet.prototype.walk = () => {
     if (this.fitness + 4 <= MAXIMUM_FITNESS) {
       this.fitness += 4;
     } else {
       this.fitness = MAXIMUM_FITNESS;
+    }
+  };
+  Pet.prototype.feed = () => {
+    if (this.hunger - 3 >= MINIMUM_HUNGER) {
+      this.hunger -= 3;
+    } else {
+      this.hunger = MINIMUM_HUNGER;
     }
   };
 }
