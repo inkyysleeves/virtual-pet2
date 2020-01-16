04d864b83d1e157ef13dc66a0696e8f5b895dfef
nathan saide <nathansaide@nathans-MacBook-Pro.local>
2020-01-16 09:30:39 +0000

    refactored and replaced the magic numbers

--- src/pet.js
+++ src/pet.js
@@ -1,13 +1,23 @@
 const pet = require("../src/pet");
+
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name, age) {
-    this.name = name;
-    this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
-    this.growUp = function() {
-        this.age += 1;
-        this.hunger += 5;
-        this.fitness -= 3;
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = MAXIMUM_FITNESS;
+  this.growUp = function() {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+  };
+  Pet.prototype.walk = function() {
+    if (this.fitness + 4 <= MAXIMUM_FITNESS) {
+      this.fitness += 4;
+    } else {
+      this.fitness = MAXIMUM_FITNESS;
     }
+  };
 }
 module.exports = Pet;
